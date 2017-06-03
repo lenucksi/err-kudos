f7feeddbcd72fb2ce17868cd3603eec39225a712
Lenucksi <lenucksi@users.noreply.github.com>
2017-06-03 13:39:57 +0200

    Allow to decrease Kudos as well

--- kudos.py
+++ kudos.py
@@ -39,6 +39,20 @@ class Kudos(BotPlugin):
                       in_reply_to=msg,
                       groupchat_nick_reply=True)
 
+    @re_botcmd(pattern=r'[\w-]+\-\-', prefixed=False, flags=re.IGNORECASE)
+    def give_kudos(self, msg, match):
+        """This gives kudos"""
+        if match:
+            line = match.group(0)
+            username = line.split(' ')[0].rstrip('--')
+            self.update_kudos(username, count=-1)
+
+            t = msg.frm.room if msg.is_group else msg.frm
+            self.send(t,
+                      'kudos updated for {}'.format(username),
+                      in_reply_to=msg,
+                      groupchat_nick_reply=True)
+    
     @botcmd(admin_only=True)
     def kudos_delete_entries(self, msg, args):
         """Deletes all entries for a user"""
